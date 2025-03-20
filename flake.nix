fe0b473994ff0b6bcc305465983585eafdd12514
Mkamono <chengyaye31+github@gmail.com>
2025-03-20 07:24:50 +0000

    update

--- flake.nix
+++ flake.nix
@@ -3,12 +3,18 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs }: {
-    packages.aarch64-darwin.default = nixpkgs.legacyPackages.aarch64-darwin.buildEnv {
+  outputs = { self, nixpkgs, home-manager} @inputs: let 
+    system = "aarch64-darwin";
+  in {
+    packages.${system}.default = nixpkgs.legacyPackages.${system}.buildEnv {
       name = "default";
-      paths = with nixpkgs.legacyPackages.aarch64-darwin; [
+      paths = with nixpkgs.legacyPackages.${system}; [
         git
         curl
         go
@@ -16,6 +22,19 @@
         gh
         mise
       ];
+    };
+    homeConfigurations = {
+      myHomeConfig = home-manager.lib.homeManagerConfiguration {
+        pkgs = import nixpkgs {
+          system = system;
+        };
+        extraSpecialArgs = {
+          inherit inputs;
+        };
+        modules = [
+          ./home.nix
+        ];
+      };
     };
   };
 }
